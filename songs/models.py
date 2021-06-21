16e91ddb827ad98b3266d5093a17fe0a063ed683
sadra <sadrataghavi1383@gmail.com>
2021-06-21 17:40:25 +0430

    created model for song

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField()
+    author = models.CharField()
+    pic = models.ImageField()
+    song = models.FileField()
