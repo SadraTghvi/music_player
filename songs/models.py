3b3c0aae98f8a2a3c4d44400dd17822b44008f6b
sadra <sadrataghavi1383@gmail.com>
2021-06-21 22:29:26 +0430

    foreign key with user added so only authenticated useres can add songs

--- songs/models.py
+++ songs/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Song(models.Model):
     title = models.CharField(max_length=100, blank=False, null=False)
-    author = models.CharField(max_length=50, blank=False, null=False)
+    author = models.ForeignKey(User, blank=False, null=False,  on_delete=models.CASCADE)
     pic = models.ImageField()
     song = models.FileField()
+
+    def __str__(self):
+        return self.title
