9f23f84fcd0b4f8fec459296fa821b4e4a02eef1
sadra <sadrataghavi1383@gmail.com>
2021-06-21 17:49:27 +0430

    models upgrated (max_length added)

--- songs/models.py
+++ songs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    title = models.CharField()
-    author = models.CharField()
+    title = models.CharField(max_length=100, blank=False, null=False)
+    author = models.CharField(max_length=50, blank=False, null=False)
     pic = models.ImageField()
     song = models.FileField()
